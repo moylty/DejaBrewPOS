7de17ac04ea5a1898ba7316f6ce58b8bea0b39af
moylty <tyl9rm0yle@gmail.com>
2022-08-22 18:16:14 +0800

    made initial input comparison part

--- part1.c
+++ part1.c
@@ -17,13 +17,22 @@ int main(void)
     Coffee macchiato; macchiato.price = 5.00;
 
     int ordering = 1;
+    const int osLengthMax = 12;
+    char orderSelection[osLengthMax];
 
     while (ordering)
     {
         printf("DejaBrew POS\n\nAvailable coffees:\nFlat White: $%.2f\nCappuccino: $%.2f\nExpresso:   $%.2f\nLong Black: $%.2f\nMacchiato:  $%.2f\n",
         flatWhite.price, cappucino.price, expresso.price, longBlack.price, macchiato.price);
 
-        scanf("%d", &ordering);
+        printf("Select a coffee to order: ");
+        fgets(orderSelection, osLengthMax, stdin);
+
+        // using strcasecmp() rather than strcmp() because it is case insensitive
+        if (!(strcasecmp(orderSelection, "flat white\n")))  // inverting result of strcasecmp because a match returns zero
+        {
+            printf("\nflat white selected\n");
+        }
     }
 
 
