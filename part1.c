85d755a59496fa2de5d98f84fbe10d6004bb49db
moylty <tyl9rm0yle@gmail.com>
2022-08-22 17:53:56 +0800

    started actual code for part 1

--- part1.c
+++ part1.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    printf("Hello, world!\n");
-    return 0;
+    struct Coffee
+    {
+        char name[12];
+        float price;
+    };
+
+    typedef struct Coffee Coffee;
+
+    Coffee flatWhite; flatWhite.price = 4.50;
+    Coffee cappucino; cappucino.price = 4.70;
+    Coffee expresso; expresso.price = 3.50;
+    Coffee longBlack; longBlack.price = 4.00;
+    Coffee macchiato; macchiato.price = 5.00;
+
+    int ordering = 1;
+
+    while (ordering)
+    {
+        printf("DejaBrew POS\n\nAvailable coffees:\nFlat White: $%.2f\nCappuccino: $%.2f\nExpresso:   $%.2f\nLong Black: $%.2f\nMacchiato:  $%.2f\n",
+        flatWhite.price, cappucino.price, expresso.price, longBlack.price, macchiato.price);
+
+        scanf("%d", &ordering);
+    }
+
+
 }
