7f9036d1506cd605c402217cc6d3d594e4fe21df
moylty <48623591+moylty@users.noreply.github.com>
2022-08-26 15:02:11 +0800

    idk

--- part2/menu.h
+++ part2/menu.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <QList>
 #include <QString>
+#include <QtWidgets>
 
 class Menu
 {
@@ -11,10 +13,13 @@ class Menu
         float exPrice = 3.50;
         float lbPrice = 4.00;
         float maPrice = 5.00;
-
+        /*
         QString fwLabel = "Flat White";
         QString caLabel = "Cappuccino";
         QString exLabel = "Expresso";
         QString lbLabel = "Long Black";
         QString maLabel = "Macchiato";
+       */
+
+      //QList<QString> menuList(30, "Empty");  // using a QList for the menu is better than a normal list because it is dynamic
 };
